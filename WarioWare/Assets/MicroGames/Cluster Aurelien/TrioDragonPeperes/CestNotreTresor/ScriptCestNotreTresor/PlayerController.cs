fec01b32dc9bf3f0f923e0a31b96fece04529cee
maelRicou <m.ricou@rubika-edu.com>
2020-12-01 13:56:42 +0000

    Le projet

---
+++ WarioWare/Assets/MicroGames/Cluster Aurelien/TrioDragonPeperes/CestNotreTresor/ScriptCestNotreTresor/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DragonPepere
+{
+    namespace CestNotreTresor
+    {
+        /// <summary>
+        /// Mael Ricou
+        /// </summary>
+
+        public class PlayerController : MonoBehaviour
+        {
+
+            [SerializeField] float speed = 10f;
+            private float moveInput;
+
+            Rigidbody2D rb;
+
+            private void Start()
+            {
+                rb = GetComponent<Rigidbody2D>();
+            }
+
+            private void Update()
+            {
+
+            }
+
+            private void FixedUpdate()
+            {
+                moveInput = Input.GetAxisRaw("Left_Joystick_X");
+                rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+            }
+        }
+    }
+}
+
+   
