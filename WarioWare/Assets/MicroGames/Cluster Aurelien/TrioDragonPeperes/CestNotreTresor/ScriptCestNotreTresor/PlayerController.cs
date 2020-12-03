f870a0e7bdebc0414b6e538e81c17d67b5ba0387
maelRicou <m.ricou@rubika-edu.com>
2020-12-03 16:02:35 +0000

    PlayerController fixed (99%)
    le perso bouge tout seul 1 fois sur 100 pendant un très court instant

--- WarioWare/Assets/MicroGames/Cluster Aurelien/TrioDragonPeperes/CestNotreTresor/ScriptCestNotreTresor/PlayerController.cs
+++ WarioWare/Assets/MicroGames/Cluster Aurelien/TrioDragonPeperes/CestNotreTresor/ScriptCestNotreTresor/PlayerController.cs
@@ -16,6 +16,8 @@ namespace Dragons_Peperes
             [SerializeField] float speed = 10f;
             private float moveInput;
 
+            [SerializeField]bool canMove;
+
             Rigidbody2D rb;
 
             private void Start()
@@ -25,10 +27,26 @@ namespace Dragons_Peperes
 
             private void Update()
             {
-                Debug.Log(Input.GetAxis("Left_Joystick_X"));
+                //Debug.Log(Input.GetAxis("Left_Joystick_X"));
+
+                if(Mathf.Abs(moveInput) < 0.05 && Mathf.Abs(moveInput) > -0.05)
+                {
+                    Debug.Log("You can't move mothafacka");
+                    speed = 0f;
+                }
+                else
+                {
+                    speed = 10f;
+                    Debug.Log("puto va la tens de mexer");
+                }
             }
 
             private void FixedUpdate()
+            {
+                MovePlayer();
+            }
+
+            private void MovePlayer()
             {
                 moveInput = Input.GetAxisRaw("Left_Joystick_X");
                 rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
