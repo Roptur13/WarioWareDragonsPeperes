34ea91b37252300eabacaa654897cc92b0732f09
Noé <noe.blanc.2k@gmail.com>
2020-12-14 23:30:08 +0100

    New package ID + packages others trios

---
+++ WarioWare/Assets/MicroGames/Cluster Aurelien/TrioACommAkuma/MiniGame3/AssetMiniGame3/ScriptMiniGame3/LevelManagerScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Testing;
+
+namespace ACommeAkuma
+{
+    namespace SaveThePirate
+    {
+        /// <summary>
+        /// PARET Paul
+        /// </summary>
+        public class LevelManagerScript : TimedBehaviour
+        {
+            [Header("Levels")]
+            public GameObject level1Prefab;
+            public GameObject level2Prefab;
+            public GameObject level3Prefab;
+
+            [Header("Difficulty")]
+            public Difficulty difficulty;
+
+
+            public override void Start()
+            {
+                base.Start(); //Do not erase this line!
+
+                switch (currentDifficulty)
+                {
+                    case Difficulty.EASY:
+                        Instantiate(level1Prefab, transform);
+                        break;
+                    case Difficulty.MEDIUM:
+                        Instantiate(level2Prefab, transform);
+                        break;
+                    case Difficulty.HARD:
+                        Instantiate(level3Prefab, transform);
+                        break;
+                }
+
+            }
+
+            //FixedUpdate is called on a fixed time.
+            public override void FixedUpdate()
+            {
+                base.FixedUpdate(); //Do not erase this line!
+
+
+            }
+
+            //TimedUpdate is called once every tick.
+            public override void TimedUpdate()
+            {
+
+            }
+
+        }
+    }
+}
